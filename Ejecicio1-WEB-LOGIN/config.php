d8bf7544e47647ac5d60d613da5c8237137f22d4
moradmt00 <53947035+moradmt00@users.noreply.github.com>
2021-05-17 11:48:37 +0200

    Update config.php

--- Ejecicio1-WEB-LOGIN/config.php
+++ Ejecicio1-WEB-LOGIN/config.php
@@ -3,9 +3,8 @@
     $user = "proyecto";  
     $password = 'root';  
     $db_name = "proyecto";  
-    $db_port = "3306";
       
-    $con = new mysqli($host, $user, $password, $db_port, $db_name);  
+    $con = mysqli_connect($host, $user, $password, $db_name);  
     if(mysqli_connect_errno()) {  
         die("Failed to connect with MySQL: ". mysqli_connect_error());  
     } 
